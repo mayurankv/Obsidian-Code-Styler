bc30b1d03ab7e81d3356e9979483e84bbcb2a662
Mayuran Visakan <mayuran.k.v@gmail.com>
2023-06-12 11:20:42 +0100

    Restructure settings interfaces

--- src/Settings.ts
+++ src/Settings.ts
@@ -1,164 +1,247 @@
-export interface CodeblockCustomizerSettings { //TODO (@mayurankv) Refactor the settings interface and respect it
-  bEnableLineNumbers: boolean;
-  bActiveCodeblockLineHighlight: boolean;
-  bActiveLineHighlight: boolean;
-  bGutterHighlight: boolean;
-  ExcludeLangs: string;
-  activeCodeBlockLineColor: string;
-  activeLineColor: string;
-  backgroundColor: string;
-  highlightColor: string;
-  gutterTextColor: string;
-  gutterBackgroundColor: string;
-  ThemeName: string;
-  SelectedTheme: string;
-  colorThemes: Array<{name: string, colors: CodeblockCustomizerColors}>;
-  alternateColors: Array<{name: string, currentColor: string, darkColor: string, lightColor: string}>;
-  bDisplayCodeBlockLanguage: boolean;
-  bDisplayCodeBlockIcon: boolean;
-  headerSettings: {
-    bHeaderBold: boolean;
-    bHeaderItalic: boolean;
-    bCodeblockLangBold: boolean;
-    bCodeblockLangItalic: boolean;
-    bAlwaysDisplayCodeblockLang: boolean;
-    bAlwaysDisplayCodeblockIcon: boolean;
-    bDefaultDarkTheme: boolean;
-    bDefaultLightTheme: boolean;
-    headerColor: string;
-    headerTextColor: string;
-    headerLineColor: string;    
-    codeBlockLangColor: string;
-    codeBlockLangBackgroundColor: string;
-  }
-}
+// Color Typing
+type RGB = `rgb(${number}, ${number}, ${number})`;
+type RGBA = `rgba(${number}, ${number}, ${number}, ${number})`;
+type HEX = `#${string}`;
+type CSS = `var(--${string})`;
+type Color = HEX | RGB | RGBA | CSS;
 
-interface CodeblockCustomizerColors {
-  activeCodeBlockLineColor: string;
-  activeLineColor: string;
-  backgroundColor: string;
-  highlightColor: string;
-  headerColor: string;
-  headerTextColor: string;
-  headerLineColor: string;
-  gutterTextColor: string;
-  gutterBackgroundColor: string;
-  codeBlockLangColor: string;
-  codeBlockLangBackgroundColor: string;
-}
-
-// dark
-export const D_ACTIVE_CODEBLOCK_LINE_COLOR = '#073642';
-export const D_ACTIVE_LINE_COLOR = '#468eeb33';
-export const D_BACKGROUND_COLOR = '#002B36';
-export const D_HIGHLIGHT_COLOR = '#054b5c';
-export const D_HEADER_COLOR = '#0a4554';
-export const D_HEADER_TEXT_COLOR = '#DADADA';
-export const D_HEADER_LINE_COLOR = '#46cced';
-export const D_GUTTER_TEXT_COLOR = '#6c6c6c';
-export const D_GUTTER_BACKGROUND_COLOR = '#073642';
-export const D_LANG_COLOR = '#000000';
-export const D_LANG_BACKGROUND_COLOR = '#008080';
-
-// light
-export const L_ACTIVE_CODEBLOCK_LINE_COLOR = '#EDE8D6';
-export const L_ACTIVE_LINE_COLOR = '#60460633';
-export const L_BACKGROUND_COLOR = '#FCF6E4';
-export const L_HIGHLIGHT_COLOR = '#E9DFBA';
-export const L_HEADER_COLOR = '#D5CCB4';
-export const L_HEADER_TEXT_COLOR = '#866704';
-export const L_HEADER_LINE_COLOR = '#EDD489';
-export const L_GUTTER_TEXT_COLOR = '#6c6c6c';
-export const L_GUTTER_BACKGROUND_COLOR = '#EDE8D6';
-export const L_LANG_COLOR = '#C25F30';
-export const L_LANG_BACKGROUND_COLOR = '#B8B5AA';
-
-export const DEFAULT_SETTINGS: CodeblockCustomizerSettings = {
-  bEnableLineNumbers: true,
-  bActiveCodeblockLineHighlight: true,
-  bActiveLineHighlight: true,
-  bGutterHighlight: false,
-  ExcludeLangs: "dataview, ad-*",
-  activeCodeBlockLineColor: D_ACTIVE_CODEBLOCK_LINE_COLOR,
-  activeLineColor: D_ACTIVE_LINE_COLOR,
-  backgroundColor: D_BACKGROUND_COLOR,
-  highlightColor: D_HIGHLIGHT_COLOR,
-  bDisplayCodeBlockLanguage: true,
-  bDisplayCodeBlockIcon: false,
-  gutterTextColor: D_GUTTER_TEXT_COLOR,
-  gutterBackgroundColor: D_GUTTER_BACKGROUND_COLOR,
-  SelectedTheme: "",
-  header: {
-    bHeaderBold: false,
-    bHeaderItalic: false,
-    bCodeblockLangBold: true,
-    bCodeblockLangItalic: true,
-    bAlwaysDisplayCodeblockLang: false,
-    bAlwaysDisplayCodeblockIcon: false,
-    bDefaultDarkTheme: false,
-    bDefaultLightTheme: false,
-    color: D_HEADER_COLOR,
-    textColor: D_HEADER_TEXT_COLOR,
-    lineColor: D_HEADER_LINE_COLOR,
-    codeBlockLangColor: D_LANG_COLOR,
-    codeBlockLangBackgroundColor: D_LANG_BACKGROUND_COLOR
+// Interface Creation
+export interface CodeblockCustomizerThemeModeColors {
+  codeblock: {
+    backgroundColor?: Color;
+    textColor?: Color;
   },
-  colorThemes: 
-  [
-    {
-      name: "Dark Theme",
-      colors: {
-        activeCodeBlockLineColor: D_ACTIVE_CODEBLOCK_LINE_COLOR,
-        activeLineColor: D_ACTIVE_LINE_COLOR,
-        backgroundColor: D_BACKGROUND_COLOR,
-        highlightColor: D_HIGHLIGHT_COLOR,
-        gutterTextColor: D_GUTTER_TEXT_COLOR,
-        gutterBackgroundColor: D_GUTTER_BACKGROUND_COLOR,
-        header: {
-          bHeaderBold: false,
-          bHeaderItalic: false,
-          bCodeblockLangBold: true,
-          bCodeblockLangItalic: true,
-          bAlwaysDisplayCodeblockLang: false,
-          bAlwaysDisplayCodeblockIcon: false,
-          bDefaultDarkTheme: true,
-          bDefaultLightTheme: false,
-          color: D_HEADER_COLOR,
-          textColor: D_HEADER_TEXT_COLOR,
-          lineColor: D_HEADER_LINE_COLOR,
-          codeBlockLangColor: D_LANG_COLOR,
-          codeBlockLangBackgroundColor: D_LANG_BACKGROUND_COLOR
-        },
-      }
+  gutter: {
+    backgroundColor?: Color;
+    textColor?: Color;
+  },
+  header: {
+    backgroundColor?: Color;
+    file: {
+      textColor?: Color;
     },
-    {
-      name: "Light Theme",
-      colors: {
-        activeCodeBlockLineColor: L_ACTIVE_CODEBLOCK_LINE_COLOR,
-        activeLineColor: L_ACTIVE_LINE_COLOR,
-        backgroundColor: L_BACKGROUND_COLOR,
-        highlightColor: L_HIGHLIGHT_COLOR,
-        gutterTextColor: L_GUTTER_TEXT_COLOR,
-        gutterBackgroundColor: L_GUTTER_BACKGROUND_COLOR,
-        header: {
-          bHeaderBold: false,
-          bHeaderItalic: false,
-          bCodeblockLangBold: true,
-          bCodeblockLangItalic: true,
-          bAlwaysDisplayCodeblockLang: false,
-          bAlwaysDisplayCodeblockIcon: false,
-          bDefaultDarkTheme: false,
-          bDefaultLightTheme: true,
-          color: L_HEADER_COLOR,
-          textColor: L_HEADER_TEXT_COLOR,
-          lineColor: L_HEADER_LINE_COLOR,
-          codeBlockLangColor: L_LANG_COLOR,
-          codeBlockLangBackgroundColor: L_LANG_BACKGROUND_COLOR
-        },        
-      }
-    }
-  ],
-  alternateColors:
-  [
-  ]
+    languageTag: {
+      backgroundColor?: Color;
+      textColor?: Color;
+    },
+    lineColor?: Color;
+  },
+  highlights: {
+    activeCodeblockLineColor?: Color;
+    activeEditorLineColor?: Color;
+    defaultColor?: Color;
+    alternativeHighlights: Record<string,Color>;
+  },
+}
+export interface CodeblockCustomizerThemeSettings {
+  codeblock: {
+    lineNumbers: boolean;
+  },
+  gutter: {
+    highlight: boolean;
+  },
+  header: {
+    file: {
+      textBold: boolean;
+      textItalic: boolean;
+    },
+    languageTag: {
+      display: boolean;
+      displayAlways: boolean;
+      textBold: boolean;
+      textItalic: boolean;
+    },
+    languageIcon: {
+      display: boolean;
+      displayAlways: boolean;
+      displayColor: boolean;
+    },
+  },
+  highlights: {
+    activeCodeblockLine: boolean;
+    activeEditorLine: boolean;
+  },
+  advanced: {
+    gradientHighlights: boolean;
+    languageBorderColor: boolean;
+    iconSize: number;
+  };
+}
+export interface CodeblockCustomizerThemeColors {
+  light: CodeblockCustomizerThemeModeColors;
+  dark: CodeblockCustomizerThemeModeColors;
+}
+export interface CodeblockCustomizerTheme {
+  settings: CodeblockCustomizerThemeSettings;
+  colors: CodeblockCustomizerThemeColors;
+}
+export interface CodeblockCustomizerSettings {
+  selectedTheme: string;
+  defaultTheme: string;
+  currentTheme: CodeblockCustomizerTheme;
+  themes: Record<string,CodeblockCustomizerTheme>;
+  excludedLangs: string;
+}
+
+// Theme Defaults
+const THEME_DEFAULT_SETTINGS: CodeblockCustomizerThemeSettings = {
+  codeblock: {
+    lineNumbers: true,
+  },
+  gutter: {
+    highlight: true,
+  },
+  header: {
+    file: {
+      textBold: false,
+      textItalic: false,
+    },
+    languageTag: {
+      display: true,
+      displayAlways: true,
+      textBold: true,
+      textItalic: true,
+    },
+    languageIcon: {
+      display: true,
+      displayAlways: true,
+      displayColor: true,
+    },
+  },
+  highlights: {
+    activeCodeblockLine: true,
+    activeEditorLine: false,
+  },
+  advanced: {
+    gradientHighlights: false,
+    languageBorderColor: false,
+    iconSize: 28,
+  },
+}
+export const THEME_FALLBACK_COLORS: CodeblockCustomizerThemeModeColors = {
+  codeblock: {
+    backgroundColor: 'var(--code-background)',
+    textColor: 'var(--code-normal)',
+  },
+  gutter: {
+    backgroundColor: 'var(--code-background)',
+    textColor: 'var(--code-comment)',
+  },
+  header: {
+    backgroundColor: 'var(--code-background)',
+    file: {
+      textColor: 'var(--code-comment)',
+    },
+    languageTag: {
+      backgroundColor: 'var(--code-background)',
+      textColor: 'var(--code-comment)',
+    },
+    lineColor: 'var(--code-background)',
+  },
+  highlights: {
+    activeCodeblockLineColor: 'var(--color-base-30)',
+    activeEditorLineColor: 'var(--color-base-20)',
+    defaultColor: 'var(--text-highlight-bg)',
+    alternativeHighlights: {},
+  },
+}
+
+// Theme Creation
+const DEFAULT_THEME = {
+  settings: THEME_DEFAULT_SETTINGS,
+  colors: {
+    light: {
+      codeblock: {},
+      gutter: {},
+      header: {
+        file: {},
+        languageTag: {},
+      },
+      highlights: {
+        alternativeHighlights: {},
+      },
+    },
+    dark: {
+      codeblock: {},
+      gutter: {},
+      header: {
+        file: {},
+        languageTag: {},
+      },
+      highlights: {
+        alternativeHighlights: {},
+      },
+    },
+  },
+}
+const SOLARIZED_THEME: CodeblockCustomizerTheme = {
+  settings: THEME_DEFAULT_SETTINGS,
+  colors: {
+    light: {
+      codeblock: {
+        backgroundColor: '#FCF6E4',
+        textColor: '#bababa',
+      },
+      gutter: {
+        backgroundColor: '#EDE8D6',
+        textColor: '#6c6c6c',
+      },
+      header: {
+        backgroundColor: '#D5CCB4',
+        file: {
+          textColor: '#866704',
+        },
+        languageTag: {
+          backgroundColor: '#B8B5AA',
+          textColor: '#C25F30',
+        },
+        lineColor: '#EDD489',
+      },
+      highlights: {
+        activeCodeblockLineColor: '#EDE8D6',
+        activeEditorLineColor: '#60460633',
+        defaultColor: '#E9DFBA',
+        alternativeHighlights: {},
+      },
+    },
+    dark: {
+      codeblock: {
+        backgroundColor: '#002b36',
+        textColor: '#bababa',
+      },
+      gutter: {
+        backgroundColor: '#073642',
+        textColor: '#6c6c6c',
+      },
+      header: {
+        backgroundColor: '#0a4554',
+        file: {
+          textColor: '#dadada',
+        },
+        languageTag: {
+          backgroundColor: '#008080',
+          textColor: '#000000',
+        },
+        lineColor: '#46cced',
+      },
+      highlights: {
+        activeCodeblockLineColor: '#073642',
+        activeEditorLineColor: '#468eeb33',
+        defaultColor: '#054b5c',
+        alternativeHighlights: {},
+      },
+    },
+  },
+}
+
+// Plugin default settings
+export const DEFAULT_SETTINGS: CodeblockCustomizerSettings = {
+  selectedTheme: '',
+  defaultTheme: 'Default',
+  currentTheme: DEFAULT_THEME,
+  themes: {
+    'Default': DEFAULT_THEME,
+    'Solarized': SOLARIZED_THEME,
+  },
+  excludedLangs: "dataview, dataviewjs, ad-*",
 }
