13ce814f295911c2efd5f371d2158a465e1689be
Mayuran Visakan <mayuran.k.v@gmail.com>
2023-08-12 16:48:22 +0100

    Add automatic packaging

--- version-bump.mjs
+++ version-bump.mjs
@@ -1,14 +1,27 @@
 import { readFileSync, writeFileSync } from "fs";
+import { exec } from "child_process";
 
-const targetVersion = process.env.npm_package_version;
+// Get new version
+const newVersion = process.env.npm_package_version;
 
-// read minAppVersion from manifest.json and bump version to target version
+// Read minAppVersion from manifest.json and bump version to target version
 let manifest = JSON.parse(readFileSync("manifest.json", "utf8"));
 const { minAppVersion } = manifest;
-manifest.version = targetVersion;
+manifest.version = newVersion;
 writeFileSync("manifest.json", JSON.stringify(manifest, null, "\t"));
 
-// update versions.json with target version and minAppVersion from manifest.json
+// Update versions.json with target version and minAppVersion from manifest.json
 let versions = JSON.parse(readFileSync("versions.json", "utf8"));
-versions[targetVersion] = minAppVersion;
+versions[newVersion] = minAppVersion;
 writeFileSync("versions.json", JSON.stringify(versions, null, "\t"));
+
+// Update CHANGELOG.md
+let changelog = readFileSync("CHANGELOG.md", "utf8");
+const lastVersion = /\[Unreleased\]: \/..\/..\/compare\/(\d+.\d+.\d+)...HEAD/.exec(changelog)?.[1] ?? "0.0.0";
+process.env.release_notes = /(?<=## \[Unreleased\])[\s\S]*?\n(?=## )/.exec(changelog)[0].trim().replace(/###/g,"#");
+writeFileSync("CHANGELOG.md",changelog.replace(/## \[Unreleased\]/,`## [Unreleased]\n\n## [${targetVersion}] - ${new Date().toISOString().slice(0, 10)}`).replace(/(?<=\[Unreleased\]: \/..\/..\/compare\/)(\d+.\d+.\d+)...HEAD/,`${targetVersion}...HEAD\n[${targetVersion}]: /../../compare/${lastVersion}...${targetVersion}`));
+
+// Push to origin
+exec(`gitall "Ready release $npm_package_version" && git tag -a $npm_package_version -F- <<EOF && git push origin $npm_package_version
+$release_notes
+EOF`,(error,stdout,stderr)=>console.log(error));
